8c678e415e8e1fbc7b326cca046c62639b5f7e23
NonPolynomial <NonPolynomial@users.noreply.github.com>
2019-07-24 18:30:03 +0200

    renaming

--- deck.js
+++ deck.js
@@ -1,15 +1,10 @@
 import future from '@mdx-deck/themes/future';
 
-import {
-  vsDarkPartial as codeTheme,
-} from 'code-surfer';
+import { vsDarkPartial as codeTheme } from 'code-surfer';
 
-import {slides as intro} from './01-intro/deck.mdx';
-import {slides as reactApp} from './02-react-app/deck.mdx';
+import { slides as intro } from './01-intro/deck.mdx';
+import { slides as reactApp } from './02-react-issue-tracker/deck.mdx';
 
-export const slides = [
-  ...intro,
-  ...reactApp,
-];
+export const slides = [...intro, ...reactApp];
 
 export const themes = [future, codeTheme];
