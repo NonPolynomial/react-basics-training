c3e8edee1fd612fa55359c6cf5ee96c48b81817c
NonPolynomial <NonPolynomial@users.noreply.github.com>
2019-08-03 22:20:05 +0200

    redux intro

--- deck.js
+++ deck.js
@@ -6,12 +6,14 @@ import { slides as intro } from './01-basics/deck.mdx';
 import { slides as reactApp } from './02-project-intro/deck.mdx';
 import { slides as propTypes } from './03-propTypes/deck.mdx';
 import { slides as hooks } from './04-hooks/deck.mdx';
+import { slides as redux } from './05-redux/deck.mdx';
 
 export const slides = [
   ...intro,
   ...reactApp,
   ...propTypes,
-  ...hooks
+  ...hooks,
+  ...redux,
 ];
 
 export const themes = [future, codeTheme];
--- helper.js
+++ helper.js
@@ -3,3 +3,14 @@ export const inlineCodeStyle = {
   marginTop: 0,
   marginBottom: 0,
 };
+
+export const splitCodeLeft = {
+  position: 'absolute',
+  height: '30vh',
+  width: '30vw',
+  margin: 'auto',
+  top: 0,
+  right: '60%',
+  bottom: 1,
+  left: 0
+};
