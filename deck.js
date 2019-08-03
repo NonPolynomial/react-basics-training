c344214d5e761099202a283bba46f0368c61ca63
NonPolynomial <NonPolynomial@users.noreply.github.com>
2019-08-03 11:07:32 +0200

    - hooks intro

--- deck.js
+++ deck.js
@@ -5,7 +5,13 @@ import { vsDarkPartial as codeTheme } from 'code-surfer';
 import { slides as intro } from './01-basics/deck.mdx';
 import { slides as reactApp } from './02-project-intro/deck.mdx';
 import { slides as propTypes } from './03-propTypes/deck.mdx';
+import { slides as hooks } from './04-hooks/deck.mdx';
 
-export const slides = [...intro, ...reactApp, ...propTypes];
+export const slides = [
+  ...intro,
+  ...reactApp,
+  ...propTypes,
+  ...hooks
+];
 
 export const themes = [future, codeTheme];
