95e6bf5dc5ef42467f0a2d0d45e4074af7d24fc6
NonPolynomial <NonPolynomial@users.noreply.github.com>
2019-08-05 04:45:52 +0200

    further readings

--- deck.js
+++ deck.js
@@ -9,6 +9,7 @@ import { slides as hooks } from './04-hooks/deck.mdx';
 import { slides as redux } from './05-redux/deck.mdx';
 import { slides as forms } from './06-forms/deck.mdx';
 import { slides as router } from './07-router/deck.mdx';
+import { slides as furtherReadings } from './99-further-reading/deck.mdx';
 
 export const slides = [
   ...intro,
@@ -18,6 +19,7 @@ export const slides = [
   ...redux,
   ...forms,
   ...router,
+  ...furtherReadings,
 ];
 
 export const themes = [future, codeTheme];
